40ee5153d58de2460540841fd387b587f63d7487
code180 <31139046+code180@users.noreply.github.com>
2024-09-08 03:09:53 +0300

    Delete lesson2/lesson_2_task_3.py

--- lesson2/lesson_2_task_3.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def square(Сторона_квадрата):
-    return math.ceil(Сторона_квадрата)
-
-num_items = int(input("Введите cторону квадрата: "))
-print(f"Площадь квадрата: {square(num_items*num_items)}")
