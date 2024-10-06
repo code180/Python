b72402f6b69ac6aaa4398e444446a14e2bfbfbe4
code180 <31139046+code180@users.noreply.github.com>
2024-10-06 21:10:55 +0300

    Update lesson_6_task_3.py

--- lesson6/lesson_6_task_3.py
+++ lesson6/lesson_6_task_3.py
@@ -8,11 +8,14 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()))
 
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+driver.get(
+    "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
 pictures = WebDriverWait(driver, 40).until(
-    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div#image-container.col-12.py-2 img#landscape")))
+    EC.presence_of_all_elements_located(
+        (By.CSS_SELECTOR, "div#image-container.col-12.py-2 img#landscape")))
 
-src = driver.find_element(By.CSS_SELECTOR, "#landscape").??????
+Src = driver.find_element(By.CSS_SELECTOR, "#landscape").get_attribute("src")
 
-print(src)
+print(Src)
+
